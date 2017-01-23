2a98afc075e8565f5e02fb82636daf6533c389cd
Steve Sun <stevesun@coupang.com>
2017-01-22 17:58:32 -0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "RandomScala"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by stevesun on 1/22/17.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
