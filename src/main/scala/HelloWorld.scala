4ea07f9b005395f1dab61fe3d0f62c0d253beaa8
Fisher Coder <fishercoder@gmail.com>
2021-11-13 15:49:35 -0800

    add a couple examples

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,19 @@
 /**
-  * Created by stevesun on 1/22/17.
-  */
+ * Created by stevesun on 1/22/17.
+ */
+
+import java.util.{Date, Locale}
+import java.text.DateFormat._
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
+    val now = new Date
+    val df = getDateInstance(LONG, Locale.GERMANY)
+    println(df format now)
+    val df1 = getDateInstance(LONG, Locale.CHINA)
+    println(df1 format now)
+    val df2 = getDateInstance(LONG, Locale.US)
+    println(df2 format now)
   }
 }
