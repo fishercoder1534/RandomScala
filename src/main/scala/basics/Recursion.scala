35499f91d590c197540cc1e78e6d891f32a9b9eb
Fisher <fishercoder@gmail.com>
2022-02-07 20:09:06 -0800

    add tail recursion examples

--- src/main/scala/basics/Recursion.scala
+++ src/main/scala/basics/Recursion.scala
@@ -32,14 +32,28 @@ object Recursion extends App {
   println(anotherFactorial(20000))
 //  use @tailrec annotation to make sure that your recursive function is truly tail recursion
 
-  def concat(aString: String, num: Int): String = {
+  def concatTailRecursion(aString: String, num: Int): String = {
     @tailrec
-    def concatHelper(aStr: String, num: Int): String = {
-      if (num <= 1) aStr
-      else concatHelper(aStr + aStr, num - 1)
+    def concatHelper(aStr: String, num: Int, accumulator: String): String = {
+      if (num <= 0) aStr
+      else concatHelper(aStr, num - 1, accumulator + aStr)
     }
 
-    concatHelper(aString, num)
+    concatHelper(aString, num, "")
   }
-  println(concat("hello ", 5))
+  var x = concatTailRecursion("hello ", 5)
+  println(x)
+  println(x.split(" ").length)
+
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def isPrimeTailRecursion(t: Int, isStillPrime: Boolean): Boolean = {
+      if (!isStillPrime) false
+      else if (t <= 1) true
+      else isPrimeTailRecursion(t - 1, n % t != 0 && isStillPrime)
+    }
+
+    isPrimeTailRecursion(n / 2, true)
+  }
+  println(isPrime(23))
 }
