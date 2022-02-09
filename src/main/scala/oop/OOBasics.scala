aa7c4177a8c1a2499aa8597a61639e4d0f8bd9e7
Fisher <fishercoder@gmail.com>
2022-02-08 16:55:10 -0800

    add more examples for OO

--- src/main/scala/oop/OOBasics.scala
+++ src/main/scala/oop/OOBasics.scala
@@ -12,6 +12,21 @@ object OOBasics extends App {
 
   val person3 = new Person2("Rachel")
   println(person3.greet("Adam"))
+
+  val writer = new Writer("John", "Doe", 2022)
+  println(writer.fullName())
+
+  val novel = new Novel(author = writer)
+  novel.authorAge()
+  println(novel.isWrittenBy(writer))
+  val writer2 = new Writer("Jane", "Doe", 2022)
+  println(novel.isWrittenBy(writer2))
+
+  val counter = new Counter()
+  println(counter.count)
+  counter.increment.print
+  counter.increment(10).print
+
 }
 
 class Person(name: String = "Steve", age: Int = 12) {
@@ -36,4 +51,47 @@ class Person2(val name: String = "Steve", val age: Int = 12) {
 //  multiple constructors
   def this(name: String) = this(name, 0)
   def this() = this("John Doe")
+}
+
+class Writer(val firstName: String, val lastName: String, val year: Int) {
+  def fullName(): Unit = {
+    println(s"My full name is: ${this.firstName} ${this.lastName}")
+  }
+}
+
+class Novel(
+    val name: String = "Gone with the Wind",
+    val year: Int = 1990,
+    val author: Writer
+) {
+  def authorAge(): Unit = {
+    println(s"${this.name}'s age is: ${this.year}")
+  }
+
+  def isWrittenBy(author: Writer) = author == this.author
+
+  def copy(newYear: Int): Novel = new Novel(name, newYear, this.author)
+}
+
+class Counter(val count: Int = 0) {
+  def increment = {
+    println("Incrementing")
+    new Counter(count + 1) //immutability, this shows that objects cannot be modified inside.
+  }
+
+  def decrement = {
+    println("Decrementing")
+    new Counter(count - 1)
+  }
+
+  def increment(n: Int): Counter = {
+    if (n <= 0) this
+    else increment.increment(n - 1)
+  }
+  def decrement(n: Int): Counter = {
+    if (n <= 0) this
+    else decrement.decrement(n - 1)
+  }
+
+  def print = println(this.count)
 }
