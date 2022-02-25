37e55971465988179d5f23bc9074f51d1c2298b9
Fisher <fishercoder@gmail.com>
2022-02-24 19:21:20 -0800

    add zip examples

--- src/main/scala/CollectionsExample.scala
+++ src/main/scala/CollectionsExample.scala
@@ -3,6 +3,12 @@ object CollectionsExample extends App {
   val list1 = List("A", "B", "C", "D")
   //apply operation to create a zip of list
   val list2 = list zip list1
-  //print list
   println(list2)
+
+  val a = List(1, 2, 3, 4)
+  val b = List("a", "b", "c")
+  val d = b.zip(a)
+  println(d)
+  val e = d.unzip
+  println(e)
 }
